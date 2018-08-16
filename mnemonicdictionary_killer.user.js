b7176fa3ebd03d394f1413f33c7314abd87a1f93
rushic24 <rushic24@gmail.com>
2018-08-16 20:34:07 +0000

    first commit

---
+++ mnemonicdictionary_killer.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Mnemonic Dictionary Killer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  An efficient UI for the site for mobile devices
+// @require http://code.jquery.com/jquery-1.12.4.min.js
+// @author       rushic24@gmail.com
+// @match        https://mnemonicdictionary.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $('.col-lg-3').empty();
+    $('.float-whats-app').remove();
+
+})();
